9ddc3cdaf3adbe6aef18390b5f29ddbe089f58eb
Della <matheusdellacrocce@hotmail.com>
2022-09-04 17:35:06 -0300

    Terminado

--- src/view/Main.java
+++ src/view/Main.java
@@ -7,13 +7,13 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] vetor = new int[1000];
+		int[] vetor = new int[1000]; // Cria um vetor com 1000 espaços
 
-		for (int i = 0; i < vetor.length; i++) {
+		for (int i = 0; i < vetor.length; i++) { // Preenche esse vetor com números randoms de 1 a 100
 			vetor[i] = (int) (Math.random() * 100) + 1;
 		}
 
-		for(int i=1;i<=2;i++) {
+		for(int i=1;i<=2;i++) { // Ativa as 2 threads com 1 e 2 e o mesmo vetor
 			threadVetor tV = new threadVetor(i, vetor);
 			tV.start();
 		}
